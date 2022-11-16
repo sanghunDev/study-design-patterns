ea5799efe7f0ca07a5b61c2009d7a1fa887041a4
sanghunDev <bsh20057@gmail.com>
2022-11-16 17:49:18 +0900

    perf(싱글톤): 멀티스레드에 안정성 확보 - double checked locking 사용 - if 와 synchronized 로 두번 체크해서 멀티 쓰레드에서 안정하게 사용 가능하다 - 인스턴스가 없는 경우를 먼저 검증하고 synchronized 를 사용하기 때문에 해당 키워드가 실행될 확률이 적어진다 - 하지만 코드가 굉장히 복잡하며 volatile 을 쓰는것을 이해하기도 복잡하다

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
@@ -2,15 +2,29 @@ package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
 
 public class Settings {
 
-    // 이른 초기화 (스레드 세이프 하다)
-    private static final Settings INSTANCE = new Settings();
+    // 더블 체킹을 위해 volatile 사용
+    private static volatile Settings instance;
 
     // 외부에서 new 키워드를 사용하지 못하게 기본 생성자에 private 설정
     private Settings() {
+
     }
 
     // 정적 팩토리 메서드 생성
     public static Settings getInstance() {
-        return INSTANCE;
+        if (instance == null) {
+            /**
+             * if 와 synchronized 로 두번 체크해서 멀티 쓰레드에서 안정하게 사용 가능하다
+             * 인스턴스가 없는 경우를 먼저 검증하고 synchronized 를 사용하기 때문에 해당 키워드가 실행될 확률이 적어진다
+             *
+             **/
+
+            synchronized (Settings.class) {
+                if (instance == null) {
+                    instance = new Settings();
+                }
+            }
+        }
+        return instance;
     }
 }
