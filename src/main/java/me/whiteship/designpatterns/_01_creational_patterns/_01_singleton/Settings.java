2d4f060ddc8204ef95fdb3382eeacd7da0615685
sanghunDev <bsh20057@gmail.com>
2022-11-16 17:41:15 +0900

    perf(싱글톤): 멀티스레드에 안정성 확보 - 이른 초기화 사용하기 (eager initialization) - 해당 객체 생성에 많은 비용이 들지 않는다면 객체를 미리 생성 해놓는다 - 단점은 굉장히 많은 리소스를 사용해서 만든다면 미리 만든다는 것 자체가 단점이 될 수 있다 (미사용 하는 경우 큰 단점)

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
@@ -2,17 +2,15 @@ package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
 
 public class Settings {
 
-    private static Settings instance;
+    // 이른 초기화 (스레드 세이프 하다)
+    private static final Settings INSTANCE = new Settings();
 
     // 외부에서 new 키워드를 사용하지 못하게 기본 생성자에 private 설정
     private Settings() {
     }
 
     // 정적 팩토리 메서드 생성
-    public static synchronized Settings getInstance() {
-        if (instance != null) {
-            instance = new Settings();
-        }
-        return instance;
+    public static Settings getInstance() {
+        return INSTANCE;
     }
 }
