de7d985be9a043f59d9b6867c80faec430c1fcfb
sanghun <bsh20057@gmail.com>
2022-11-18 00:12:25 +0900

    perf(싱글톤): 안전하고 단순하게 구현하는 방법 - ENUM 으로 인스턴스를 생성하면 리플렉션으로 인스턴스 생성을 막을 수 있다 - 또한 직렬화와 역직렬화에도 안전하다 - 가장 간단하고 안전하게 싱글톤을 구현하는 방법 중 하나이다
    - 단점은 미리 만들어진다는것
    (클래스를 로딩하는 순간 인스턴스가 만들어졌다는것 : 사용할 때 만들어 지는게 아님)

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
@@ -1,10 +1,12 @@
 package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
 
 import java.io.*;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 public class App {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        Settings instance = Settings.getInstance();
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException, IOException, ClassNotFoundException {
+        Settings instance = Settings.INSTANCE;
         Settings instance1 = null;
 
         try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))){
@@ -16,5 +18,16 @@ public class App {
         }
 
         System.out.println(instance == instance1);
+
+        Settings instance2 = null;
+        //ENUM 은 리플랙션으로 인스턴스 생성 불가(따라서 싱글톤을 깰 수 없게 되며 오류가 출력된다)
+        Constructor<?>[] declaredConstructors = Settings.class.getDeclaredConstructors();
+        for (Constructor constructor : declaredConstructors) {
+            constructor.setAccessible(true);
+            instance2 = (Settings) constructor.newInstance("INSTANCE");
+        }
+
+        System.out.println(instance == instance1);
+        System.out.println(instance == instance2);
     }
 }
--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/Settings.java
@@ -1,24 +1,7 @@
 package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
 
-import java.io.Serializable;
+public enum Settings {
 
-public class Settings implements Serializable {
+    INSTANCE;
 
-    // 외부에서 new 키워드를 사용하지 못하게 기본 생성자에 private 설정
-    private Settings() {
-
-    }
-
-    private static class SettingsHolder {
-        private static final Settings INSTANCE = new Settings();
-    }
-
-    // 정적 팩토리 메서드 생성
-    public static Settings getInstance() {
-        return SettingsHolder.INSTANCE;
-    }
-
-    protected Object readResolve() {
-        return getInstance();
-    }
 }
