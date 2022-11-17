657cf1a589b78b4499103744b84a8d48405e7416
sanghun <bsh20057@gmail.com>
2022-11-17 23:50:57 +0900

    perf(싱글톤): 싱글톤 패턴 깨뜨리기 - 리플렉션 사용하기 - 리플렉션을 사용해 인스턴스 생성 가능 - 기존 인스턴스와 다른 인스턴스가 생성되어 결과값 false

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_01_singleton/App.java
@@ -1,8 +1,15 @@
 package me.whiteship.designpatterns._01_creational_patterns._01_singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings instance = Settings.getInstance();
-        System.out.println(instance == Settings.getInstance());
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        Settings instance1 = constructor.newInstance();
+        System.out.println(instance == instance1);
     }
 }
