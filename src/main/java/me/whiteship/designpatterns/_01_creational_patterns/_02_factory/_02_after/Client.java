967b1ad8861391d7c9ba375328be1b40efc4341c
sanghun <bsh20057@gmail.com>
2022-11-22 23:42:29 +0900

    perf(팩토리 메소드 패턴): 팩토리 메소드 적용 예제 - 2 - 인터페이스 타입으로 받아서 구체적인 로직을 처리하는 부분 작성 - 클라이언트 코드를 최소한으로 변경 가능하게 처리

--- src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_02_factory/_02_after/Client.java
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_02_factory/_02_after/Client.java
@@ -3,10 +3,14 @@ package me.whiteship.designpatterns._01_creational_patterns._02_factory._02_afte
 public class Client {
 
     public static void main(String[] args) {
-        Ship sanghun = new SanghunShipFactory().orderShip("sanghun", "sanghun@mail.com");
-        System.out.println("sanghun = " + sanghun);
+        Client client = new Client();
+        //새로운 배를 제작한다면 호출부만 추가됨
+        client.print(new SanghunShipFactory(), "sanghun", "sanghun@mail.com");
+        client.print(new BlackShipFactory(), "blackship", "blackship@mail.com");
+    }
 
-        Ship blackship = new BlackShipFactory().orderShip("blackship", "blackship@mail.com");
-        System.out.println("blackship = " + blackship);
+    // 해당 부분을 클래스로 따로 빼낸다면 DI
+    private void print(ShipFactory shipFactory, String name, String email) {
+        System.out.println(shipFactory.orderShip(name, email));
     }
 }
