4d378e16e66fcabb1c9c40ac5ee1736939838bbe
sanghun <bsh20057@gmail.com>
2022-12-02 00:02:59 +0900

    perf(빌더패턴): 빌더 패턴 lombok 예제 - 롬복에서 제공하는 Builder 를 사용

---
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_04_builder/_03_java/LombokExample.java
@@ -0,0 +1,21 @@
+package me.whiteship.designpatterns._01_creational_patterns._04_builder._03_java;
+
+import lombok.Builder;
+
+@Builder
+public class LombokExample {
+
+    private String title;
+
+    private int nights;
+
+    private int days;
+
+    public static void main(String[] args) {
+        LombokExample trip = LombokExample.builder()
+                .title("여행")
+                .nights(1)
+                .days(2)
+                .build();
+    }
+}
