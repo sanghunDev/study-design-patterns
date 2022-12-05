d7be288481af9a4b0d351e7ef872de1cb85202e8
sanghunDev <bsh20057@gmail.com>
2022-12-05 16:24:03 +0900

    perf(프로토타입 패턴): 프로토타입 패턴 적용 전 예제 - 두번째 GithubIssue를 만들려면 일련의 과정을 복사해서 만들어야함 -> 기존 인스턴스를 프로토 타입으로 만들어서 사용하도록 변경하면 재사용성 증가

---
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_05_prototype/_01_before/App.java
@@ -0,0 +1,16 @@
+package me.whiteship.designpatterns._01_creational_patterns._05_prototype._01_before;
+
+public class App {
+    public static void main(String[] args) {
+        GithubRepository repository = new GithubRepository();
+        repository.setUser("sanghun");
+        repository.setName("live-study");
+
+        GithubIssue githubIssue = new GithubIssue(repository);
+        githubIssue.setId(1);
+        githubIssue.setTitle("1주차 과제");
+
+        String url = githubIssue.getUrl();
+        System.out.println("url = " + url);
+    }
+}
