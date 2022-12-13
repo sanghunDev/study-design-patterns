e2f5c1b5abd81ca7ada75523e1af3f89772d7f7b
sanghun <bsh20057@gmail.com>
2022-12-13 22:46:38 +0900

    perf(구조적인 패턴): 자바 적용 예제 - Arrays.asList - 배열을 리스트로 변환할때 흔히 사용하는 방법 - 가변인자를 넘겨주면 리스트 타입으로 변환 시켜준다 (배열을 전달 해줬지만 리스트 타입으로 변환되어 받을 수 있다)

---
+++ src/main/java/me/whiteship/designpatterns/_01_creational_patterns/_06_structuralPatterns/_03_java/AdapterInJava.java
@@ -0,0 +1,11 @@
+package me.whiteship.designpatterns._01_creational_patterns._06_structuralPatterns._03_java;
+
+import java.util.*;
+
+public class AdapterInJava {
+    public static void main(String[] args) {
+        //배열을 넘기지만 리스트로 반환됨
+        List<String> strings = Arrays.asList("1", "2", "3");
+
+    }
+}
