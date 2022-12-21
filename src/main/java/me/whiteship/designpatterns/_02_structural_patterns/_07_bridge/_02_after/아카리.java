afdbcda6e0f9706fbfe165e92de2511f730f5ddb
sanghun <bsh20057@gmail.com>
2022-12-21 23:17:26 +0900

    perf(브릿지 패턴): 브릿지 패턴 적용 - 1 - 이론적으로는 클라이언트 코드가 추상화 된 구조로 변경됨 - 새로운 챔피언이 추가 되더라도 클래스가 기하급수적으로 늘어나지 않고 간편하게 생성가능
    - Champion 은 Abstraction
    - Skin 은 Implementation
    - 각각의 Skin(KDA, PoolParty 등,,) 은 Concrete Implementation
    - 각각의 Champion(아리, 아카리 등,,) 은 Refined Abstraction

---
+++ src/main/java/me/whiteship/designpatterns/_02_structural_patterns/_07_bridge/_02_after/아카리.java
@@ -0,0 +1,7 @@
+package me.whiteship.designpatterns._02_structural_patterns._07_bridge._02_after;
+
+public class 아카리 extends DefaultChampion {
+    public 아카리(Skin skin) {
+        super(skin, "아카리");
+    }
+}
